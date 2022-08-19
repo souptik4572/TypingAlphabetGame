dd6aa153ff3ceec4179bc01519ee389f1c94ec61
Souptik Sarkar <souptiksarkar4572@gmail.com>
2022-08-19 09:11:54 +0530

    feat: marked failure class

--- src/components/Display/Display.tsx
+++ src/components/Display/Display.tsx
@@ -1,5 +1,5 @@
 import './Display.css';
-import { DisplayProps } from '../../types/props'
+import { DisplayProps } from '../../types/props';
 
 const SUCCESS_CLASS = 'success';
 const FAILURE_CLASS = 'failure';
@@ -7,7 +7,9 @@ const FAILURE_CLASS = 'failure';
 const Display = ({ textValue }: DisplayProps) => {
 	return (
 		<div className='Display'>
-			<h1 className={SUCCESS_CLASS}>{textValue}</h1>
+			<h1 className={textValue !== 'Failure!' ? SUCCESS_CLASS : FAILURE_CLASS}>
+				{textValue}
+			</h1>
 		</div>
 	);
 };
